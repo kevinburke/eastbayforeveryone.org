e45d1d479a3d0bf12ee3e78b1f6c4d2198874b6e
Kevin Burke <kevin@burke.dev>
2022-02-15 22:05:22 -0800

    update mysql backup scripts
    These ones should use less disk space which should give us a little
    bit more headway.

--- deployment/roles/mysql/templates/backup.sh
+++ deployment/roles/mysql/templates/backup.sh
@@ -3,11 +3,14 @@
 set -euo pipefail
 
 main() {
-    local -r backup_file="mysql-$(uname -n)-$(date +%Y-%m-%d_%H-%M-%S)"
-    /usr/bin/mysqldump --all-databases --single-transaction --user=root > "/root/tmp/$backup_file"
+    local -r backup_file="mysql-$(uname -n)-$(date +%Y-%m-%d_%H-%M-%S).zst"
+    /usr/bin/mysqldump --all-databases --compression-algorithms=zstd \
+        --compress \
+        --single-transaction --tz-utc \
+        --user=root > "{{ mysql_backup_directory }}/$backup_file"
     {% if tarsnap_enabled %}
     /usr/local/bin/tarsnap --print-stats -c \
-        -f "$backup_file" "/root/tmp/$backup_file"
+        -f "$backup_file" "{{ mysql_backup_directory }}/$backup_file"
     {% endif %}
 }
 
